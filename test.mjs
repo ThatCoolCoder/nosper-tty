de22a57df3e39587cb71bfdd7e9c7d2a576f5d45
ThatCoolCoder <ThatCoolCoder@users.noreply.github.com>
2023-11-16 10:31:02 +1030

    slight experimentation

--- test.mjs
+++ test.mjs
@@ -1,8 +1,6 @@
 import readlineSync from 'readline-sync';
 import { createInterface } from 'readline';
 
-const input = process.stdin;
-
 const rl = createInterface({
     input: process.stdin,
     output: process.stdout,
@@ -15,31 +13,13 @@ rl._addHistory = function() {
     return line;
 }
 
-rl.history.push.apply(rl.history,["toilet"]);      
+rl.history.push("toilet");      
 
-
-input.setEncoding('utf8');
-
-// input.on('data', (char) => {
-//     char = char.toString()
-//     switch (char) {
-//         case '\n':
-//         case '\r':
-//         case '\u0004':
-//             stdin.removeListener('data', onData)
-//             break
-//         case '\u0004':
-//             stdin.removeListener('data', onData)
-//             break
-//         default:
-//             process.stdout.write(char);
-//             break;
-//     }
-// });
+process.stdin.setEncoding('utf8');
 
 process.stdout.write("> ");
 
 rl.on('line', (input) => {
-    console.log("ball");
+    console.log("you said " + input);
     rl.prompt();
 });
